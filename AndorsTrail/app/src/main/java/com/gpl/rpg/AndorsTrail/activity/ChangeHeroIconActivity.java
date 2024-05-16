4227c7a08a2e31e593cc58026c6da9c560bfb33c
Raphi <113795944+RaphiHD@users.noreply.github.com>
2024-05-16 15:12:38 +0200

    Added package and imports
    Probably more imports than needed, again, just copy-pasted from LevelUpActivity. Unused imports can be removed later.

--- AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/activity/ChangeHeroIconActivity.java
+++ AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/activity/ChangeHeroIconActivity.java
@@ -1,3 +1,22 @@
+package com.gpl.rpg.AndorsTrail.activity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.gpl.rpg.AndorsTrail.AndorsTrailApplication;
+import com.gpl.rpg.AndorsTrail.R;
+import com.gpl.rpg.AndorsTrail.context.ControllerContext;
+import com.gpl.rpg.AndorsTrail.context.WorldContext;
+import com.gpl.rpg.AndorsTrail.controller.ActorStatsController;
+import com.gpl.rpg.AndorsTrail.controller.Constants;
+import com.gpl.rpg.AndorsTrail.model.actor.Player;
+import com.gpl.rpg.AndorsTrail.util.ThemeHelper;
+
 public final class ChangeHeroIconActivity extends AndorsTrailBaseActivity {
   private WorldContext world;
   private ControllerContext controllers;
